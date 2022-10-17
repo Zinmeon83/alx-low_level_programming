6bca3cd2077b1f82512e8274423f1337401e440a
Simon <zinmeon83@gmail.com>
2022-10-17 13:32:48 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -28,7 +28,8 @@ int main(void)
 						if (!(a == '8' && b == '9'))
 						{
 						putchar(',');
-						putchar(' ');																		 }	
+						putchar(' ');
+						}	
 						}
 					b++;
 					}
