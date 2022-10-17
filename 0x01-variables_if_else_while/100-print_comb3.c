38e65e9c04bd4b55234f40dd074bb27b661d7891
Simon <zinmeon83@gmail.com>
2022-10-17 13:41:03 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,25 +16,24 @@ int main(void)
 {
 
 		int a, b;
-
 		for (a = '0' ; a <= '8' ; )
+		{
+			for (b = '0' ; b <= '9' ; )
 			{
-				for (b = '0' ; b <= '9' ; )
+				if (a < b)
+				{
+					putchar(a);
+					putchar(b);
+					if (!(a == '8' && b == '9'))
 					{
-						if (a < b)
-						{
-							putchar(a);
-							putchar(b);
-						if (!(a == '8' && b == '9'))
-						{
 						putchar(',');
 						putchar(' ');
-						}
-						}
-					b++;
 					}
-				a++;
+				}
+				b++;
 			}
+			a++;
+		}
 
 	putchar('\n');
 
