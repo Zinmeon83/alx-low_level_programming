6142c7c6c66ec8569b3977684a8f27398767867f
Simon <zinmeon83@gmail.com>
2022-10-18 16:34:41 -0700

    Using _putchar in nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <main.h>
+
+/**
+ * main - prints the string "_putchar"
+ *
+ * Return: 0 on success.
+ *
+ */
+
+
+int main(void)
+
+{
+	char *str;
+
+	int i;
+
+	str = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+
+}
